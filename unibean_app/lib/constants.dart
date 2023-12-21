6a4301c4b051f7cc3d00539b5b616883d3edea50
ownhieu <ownhieu0804@gmail.com>
2023-12-21 21:30:30 +0700

    Done Splash, Onboarding and Welcome Screen

---
+++ unibean_app/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kAnimationDuration = Duration(milliseconds: 200);
+
+const kPrimaryColor = Color(0xff2ecc71);
+const kLowTextColor = Color(0xff989da3);
---
+++ unibean_app/lib/presentation/screens/splash/components/body.dart
@@ -0,0 +1,269 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:unibean_app/constants.dart';
+import 'package:unibean_app/presentation/screens/welcome/welcome_screen.dart';
+
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int pageIndex = 0;
+  PageController pageController = PageController(initialPage: 0);
+  List<Map<String, String>> splashData = [
+    {
+      'image': 'assets/images/onboarding1.png',
+      'title': 'Xin chào!',
+      'content':
+          'Tìm kiếm nững thông tin hữu ích,\n những hoạt động thú vị và\n những phần quà đặc biệt.',
+      'button': 'Tiếp tục'
+    },
+    {
+      'image': 'assets/images/onboarding2.png',
+      'title': 'Nhận Bean mọi nơi!',
+      'content':
+          'Thu nhập các điểm thưởng\n và đổi lấy những phần quà hấp dẫn!.',
+      'button': 'Tiếp tục'
+    },
+    {
+      'image': 'assets/images/onboarding3.png',
+      'title': 'Ưu Đãi đọc quyền',
+      'content':
+          'Bạn sẽ tìm thấy những ưu đãi cũng như\n các phần quà đặc biệt chỉ có tại đây.',
+      'button': 'Bắt đầu!'
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 375;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
+    double baseHeight = 812;
+    double hem = MediaQuery.of(context).size.height / baseHeight;
+
+    return PageView.builder(
+      controller: pageController,
+      onPageChanged: (value) {
+        setState(() {
+          pageIndex = value;
+        });
+      },
+      itemCount: splashData.length,
+      itemBuilder: (context, index) {
+        if (pageIndex == 2) {
+          return SafeArea(
+              child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  splashData[index]['image']!,
+                ),
+              ),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 510 * hem,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(splashData.length,
+                      (index) => buildDot(fem, index: index)),
+                ),
+                SizedBox(
+                  height: 25 * hem,
+                ),
+                Text(
+                  splashData[index]['title']!,
+                  style: GoogleFonts.nunito(
+                      textStyle: TextStyle(
+                          fontSize: 20 * ffem,
+                          fontWeight: FontWeight.w900,
+                          height: 1.3625 * ffem / fem,
+                          color: Colors.black)),
+                ),
+                SizedBox(
+                  height: 20 * hem,
+                ),
+                SizedBox(
+                  height: 60 * hem,
+                  child: Text(
+                    splashData[index]['content']!,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.nunito(
+                        textStyle: TextStyle(
+                            fontSize: 15 * ffem,
+                            fontWeight: FontWeight.w600,
+                            height: 1.3625 * ffem / fem,
+                            color: kLowTextColor)),
+                  ),
+                ),
+                SizedBox(
+                  height: 20 * hem,
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WelcomeScreen(),
+                      ));
+                  },
+                  child: Container(
+                    width: 270 * fem,
+                    height: 45 * hem,
+                    decoration: BoxDecoration(
+                        color: kPrimaryColor,
+                        borderRadius: BorderRadius.circular(23 * fem)),
+                    child: Center(
+                      child: Text(
+                        splashData[index]['button']!,
+                        style: GoogleFonts.nunito(
+                            textStyle: TextStyle(
+                                fontSize: 17 * ffem,
+                                fontWeight: FontWeight.w600,
+                                height: 1.3625 * ffem / fem,
+                                color: Colors.white)),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ));
+        }
+        return SafeArea(
+            child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                splashData[index]['image']!,
+              ),
+            ),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 510 * hem,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                    splashData.length, (index) => buildDot(fem, index: index)),
+              ),
+              SizedBox(
+                height: 25 * hem,
+              ),
+              Text(
+                splashData[index]['title']!,
+                style: GoogleFonts.nunito(
+                    textStyle: TextStyle(
+                        fontSize: 20 * ffem,
+                        fontWeight: FontWeight.w900,
+                        height: 1.3625 * ffem / fem,
+                        color: Colors.black)),
+              ),
+              SizedBox(
+                height: 20 * hem,
+              ),
+              SizedBox(
+                height: 60 * hem,
+                child: Text(
+                  splashData[index]['content']!,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.nunito(
+                      textStyle: TextStyle(
+                          fontSize: 15 * ffem,
+                          fontWeight: FontWeight.w600,
+                          height: 1.3625 * ffem / fem,
+                          color: kLowTextColor)),
+                ),
+              ),
+              SizedBox(
+                height: 20 * hem,
+              ),
+              TextButton(
+                onPressed: () {
+                  if (pageIndex == 0) {
+                    setState(() {
+                      pageIndex += 1;
+                    });
+                    pageController.animateToPage(pageIndex,
+                        duration: kAnimationDuration, curve: Curves.linear);
+                  } else if (pageIndex == 1) {
+                    setState(() {
+                      pageIndex += 1;
+                    });
+                    pageController.animateToPage(pageIndex,
+                        duration: kAnimationDuration, curve: Curves.linear);
+                  } 
+                },
+                child: Container(
+                  width: 270 * fem,
+                  height: 45 * hem,
+                  decoration: BoxDecoration(
+                      color: kPrimaryColor,
+                      borderRadius: BorderRadius.circular(23 * fem)),
+                  child: Center(
+                    child: Text(
+                      splashData[index]['button']!,
+                      style: GoogleFonts.nunito(
+                          textStyle: TextStyle(
+                              fontSize: 17 * ffem,
+                              fontWeight: FontWeight.w600,
+                              height: 1.3625 * ffem / fem,
+                              color: Colors.white)),
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WelcomeScreen(),
+                      ));
+                },
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 20 * hem),
+                  child: Text(
+                    'Bỏ qua',
+                    style: GoogleFonts.nunito(
+                        textStyle: TextStyle(
+                            fontSize: 13 * ffem,
+                            fontWeight: FontWeight.w900,
+                            height: 1.3625 * ffem / fem,
+                            color: kPrimaryColor)),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+      },
+    );
+  }
+
+  AnimatedContainer buildDot(double fem, {required int index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: EdgeInsets.only(right: 5 * fem),
+      height: 6 * fem,
+      width: 20 * fem,
+      decoration: BoxDecoration(
+        color: pageIndex == index ? kPrimaryColor : const Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
+      ),
+    );
+  }
+}
