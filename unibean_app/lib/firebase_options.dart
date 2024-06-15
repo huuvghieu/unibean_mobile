5c4c3abfd1ef180b549757656221870c70cbee83
ownhieu <ownhieu0804@gmail.com>
2024-06-15 12:43:02 +0700

    fix format

--- unibean_app/lib/firebase_options.dart
+++ unibean_app/lib/firebase_options.dart
@@ -15,48 +15,42 @@ import 'package:flutter/foundation.dart'
 /// );
 /// ```
 class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
-    }
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return android;
-      case TargetPlatform.iOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.linux:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      default:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions are not supported for this platform.',
-        );
-    }
-  }
+  // static FirebaseOptions get currentPlatform {
+  //   if (kIsWeb) {
+  //     throw UnsupportedError(
+  //       'DefaultFirebaseOptions have not been configured for web - '
+  //       'you can reconfigure this by running the FlutterFire CLI again.',
+  //     );
+  //   }
+  //   switch (defaultTargetPlatform) {
+  //     case TargetPlatform.android:
+  //       return android;
+  //     case TargetPlatform.iOS:
+  //       throw UnsupportedError(
+  //         'DefaultFirebaseOptions have not been configured for ios - '
+  //         'you can reconfigure this by running the FlutterFire CLI again.',
+  //       );
+  //     case TargetPlatform.macOS:
+  //       throw UnsupportedError(
+  //         'DefaultFirebaseOptions have not been configured for macos - '
+  //         'you can reconfigure this by running the FlutterFire CLI again.',
+  //       );
+  //     case TargetPlatform.windows:
+  //       throw UnsupportedError(
+  //         'DefaultFirebaseOptions have not been configured for windows - '
+  //         'you can reconfigure this by running the FlutterFire CLI again.',
+  //       );
+  //     case TargetPlatform.linux:
+  //       throw UnsupportedError(
+  //         'DefaultFirebaseOptions have not been configured for linux - '
+  //         'you can reconfigure this by running the FlutterFire CLI again.',
+  //       );
+  //     default:
+  //       throw UnsupportedError(
+  //         'DefaultFirebaseOptions are not supported for this platform.',
+  //       );
+  //   }
+  // }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBHoQmuBB1DzIbKZhtj0rvrrctPSfPl0hU',
-    appId: '1:804634450758:android:7db0d1ecf115769880a347',
-    messagingSenderId: '804634450758',
-    projectId: 'unibean-414815',
-    storageBucket: 'unibean-414815.appspot.com',
-  );
+
 }
